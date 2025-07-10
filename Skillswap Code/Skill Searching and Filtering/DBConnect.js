0767dda3e248cdc6b9a86aacedbd1e438fd7fef1
ddioneda <dylanjames.dioneda@gmail.com>
2025-07-10 13:17:30 -0400

    Revert "Add files via upload"
    This reverts commit 5d7db08676de847db3b2efee3c5f762db9ba9897.

--- Skillswap Code/Skill Searching and Filtering/DBConnect.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
