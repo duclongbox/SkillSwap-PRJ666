b9607f2ef00004639922f5189d577c2b9a697858
DLong <duclongbox@gmail.com>
2025-08-04 21:25:37 -0400

    add log

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -28,6 +28,7 @@ passport.serializeUser((user,done)=>{
 passport.deserializeUser(async (id,done)=>{
     try {
         const user = await User.findById(id);
+        console.log('Deserialized user:', user);
         done(null,user);
     }catch(error){
         done(error);
